51f2b529586014c54a4845eaac7c3d8082ad1c4f
khaled sabry <khaledsab1997@gmail.com>
2018-04-23 11:10:55 +0200

    Revert "sdfdsf"
    This reverts commit 058cfef1626c8d4ac8121d79ae2e3df0494e5ed7.

--- launch-Game-Debug.cmd
+++
@@ -1,9 +0,0 @@
-cd E:/Project/Project/Game/
-set PATH=;%PATH%
-
-if [%1]==[--debugger] (
-	SHIFT
-)
-
-"E:\Project\Project\Debug\Game.exe"  
-pause
--- launch-Game-MinSizeRel.cmd
+++
@@ -1,9 +0,0 @@
-cd E:/Project/Project/Game/
-set PATH=;%PATH%
-
-if [%1]==[--debugger] (
-	SHIFT
-)
-
-"E:\Project\Project\MinSizeRel\Game.exe"  
-pause
